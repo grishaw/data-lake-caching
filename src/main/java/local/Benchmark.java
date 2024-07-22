2f5c6d1894bb50a69e560cf2e79e89db5fc868c5
Grisha Weintraub <grisha.weintraub@ibm.com>
2024-07-22 20:26:29 +0300

    add benchmark for local case

--- src/main/java/local/Benchmark.java
+++ src/main/java/local/Benchmark.java
@@ -1,25 +1,71 @@
 package local;
 
-import org.apache.avro.generic.GenericData;
+import org.apache.commons.lang3.tuple.Pair;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Benchmark {
 
+
+    static List<Pair<ArrayList<Pair<Integer, Integer>>, Set<Integer>>> cache;
+
     public static void main(String[] args) {
+        runBenchmark(10, 1_000_000, 100_000, 1000, 3);
+    }
+
+    static void runBenchmark(int numOfColumns, int numOfRecords, int numOfFiles, int numOfQueries, int numOfIterations){
+
+        //TODO
+        // - verify we get the same result for cache and no-cache (use records number)
+        // - organize the flow to be simple and readable
+        // - add relevant classes - for table, interval, record, cache
+        // - do not use static cache, init per test
+        // - rename benchmark package to "cloud"
+        // - try different params
+        // - add cache data structure type (basic/enhanced/spatial type) and cache replacement type (unlimited, policy type)
+
+        List<Pair<Long, Long>> resultsTimes = new LinkedList<>();
+        List<Integer> resultsCoverageWithCache = new LinkedList<>();
+
+        for (int j=0; j<numOfIterations; j++) {
+
+            HashMap<Integer, List<ArrayList<Integer>>> table = createRandomTable(numOfColumns, numOfRecords, numOfFiles);
+            cache = new LinkedList<>();
+
+            long start = System.currentTimeMillis();
+            for (int i = 0; i < numOfQueries; i++) {
+                runRandomQuery(table, false);
+            }
+            long end = System.currentTimeMillis();
+
+            long start2 = System.currentTimeMillis();
+            for (int i = 0; i < numOfQueries; i++) {
+                resultsCoverageWithCache.add(runRandomQuery(table, true));
+            }
+            long end2 = System.currentTimeMillis();
+
+            System.out.println("iteration " + j + " : without cache = " + (end - start) + ", with cache = " + (end2 - start2));
+
+            resultsTimes.add(Pair.of((end - start),(end2 - start2)));
+        }
+
+        System.out.println(resultsTimes);
+        System.out.println(resultsCoverageWithCache);
+
+        System.out.println("--------------------------");
+        System.out.println("without cache : " + resultsTimes.stream().mapToLong(p -> p.getLeft()).average());
+        System.out.println("with cache : " + resultsTimes.stream().mapToLong(p -> p.getRight()).average());
+        System.out.println("cache hits : " + (resultsCoverageWithCache.stream().filter(r -> r != null).count() / numOfIterations));
 
     }
 
-    static HashMap<Integer, List<List<Double>>> createRandomTable(int numOfColumns, int numOfRecords, int numOfFiles){
+    static HashMap<Integer, List<ArrayList<Integer>>> createRandomTable(int numOfColumns, int numOfRecords, int numOfFiles){
 
-        HashMap<Integer, List<List<Double>>> result = new HashMap<>();
+        HashMap<Integer, List<ArrayList<Integer>>> result = new HashMap<>();
 
         for (int i=0; i<numOfRecords; i++){
-            List<Double> curRecord = generateRecord(numOfColumns);
+            ArrayList<Integer> curRecord = generateRecord(numOfColumns);
             int curPartition = ThreadLocalRandom.current().nextInt(numOfFiles);
             result.computeIfAbsent(curPartition, x -> new LinkedList<>()).add(curRecord);
         }
@@ -27,14 +73,124 @@ public class Benchmark {
         return result;
     }
 
-    static List<Double> generateRecord(int numOfColumns){
-        List<Double> result = new ArrayList<>(numOfColumns);
+    static ArrayList<Integer> generateRecord(int numOfColumns){
+        ArrayList<Integer> result = new ArrayList<>(numOfColumns);
 
         for (int i=0; i<numOfColumns; i++){
-            result.add(ThreadLocalRandom.current().nextDouble(Double.MIN_VALUE, Double.MAX_VALUE));
+            result.add(ThreadLocalRandom.current().nextInt(Integer.MIN_VALUE, Integer.MAX_VALUE));
         }
 
         return result;
+    }
+
+    static ArrayList<Pair<Integer, Integer>> generateInterval (int totalTerms, double percentageOfNonEmptyTerms){
+        ArrayList<Pair<Integer, Integer>> result = new ArrayList<>(totalTerms);
+
+        for (int i=0; i < totalTerms; i++){
+
+            double randomValue =  ThreadLocalRandom.current().nextDouble();
+
+            // non-empty case - generate random term
+            if (randomValue <= percentageOfNonEmptyTerms){
+                int random1 = ThreadLocalRandom.current().nextInt(Integer.MIN_VALUE, Integer.MAX_VALUE);
+                int random2 = ThreadLocalRandom.current().nextInt(Integer.MIN_VALUE, Integer.MAX_VALUE);
+
+                result.add(Pair.of(Math.min(random1, random2), Math.max(random1, random2)));
+            }else{
+                // no term - put min/max
+                result.add(Pair.of(Integer.MIN_VALUE, Integer.MAX_VALUE));
+            }
+        }
+
+        return result;
+    }
+
+    static Integer runRandomQuery(HashMap<Integer, List<ArrayList<Integer>>> table, boolean useCache){
+        int numOfColumns = table.values().stream().findFirst().get().get(0).size();
+        int numOfFiles = table.keySet().size();
+        double percentageOfNonEmptyTerms = ThreadLocalRandom.current().nextDouble();
+
+        ArrayList<Pair<Integer, Integer>> interval = generateInterval(numOfColumns, percentageOfNonEmptyTerms);
+
+        Set<Integer> coverage = new HashSet<>();
+        Set<Integer> minCoverage = null;
+
+        if (useCache){
+            minCoverage = getMinCoverage(cache, interval);
+        }
+
+        if (minCoverage == null) {
+
+            for (Map.Entry<Integer, List<ArrayList<Integer>>> entry : table.entrySet()) {
+                for (ArrayList<Integer> record : entry.getValue()) {
+                    if (Benchmark.intervalContainsRecord(interval, record)) {
+                        if (useCache) {
+                            coverage.add(entry.getKey());
+                        }
+                    }
+                }
+            }
+
+        }else{
+            for (int fileNum : minCoverage){
+                for (ArrayList<Integer> record : table.get(fileNum)) {
+                    if (Benchmark.intervalContainsRecord(interval, record)) {
+                        coverage.add(fileNum);
+                    }
+                }
+            }
+        }
+
+        if (useCache && coverage.size() < numOfFiles * 0.7){
+            cache.add(Pair.of(interval, coverage));
+        }
+
+        return minCoverage == null ? null : minCoverage.size();
+
+    }
+
+    static boolean intervalContainsRecord(ArrayList<Pair<Integer, Integer>> interval, ArrayList<Integer> record){
+        if (interval.size() != record.size()){
+            throw new IllegalArgumentException("interval and record should be of the same size!");
+        }
+
+        for (int i=0; i<interval.size(); i++){
+            if (record.get(i) < interval.get(i).getLeft() || record.get(i) > interval.get(i).getRight()){
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
+    static boolean intervalContainsInterval(ArrayList<Pair<Integer, Integer>> interval1, ArrayList<Pair<Integer, Integer>> interval2){
+        if (interval1.size() != interval2.size()){
+            throw new IllegalArgumentException("intervals should be of the same size!");
+        }
+
+        for (int i=0; i<interval1.size(); i++){
+            if (interval2.get(i).getLeft() < interval1.get(i).getLeft() || interval2.get(i).getRight() > interval1.get(i).getRight()){
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
+    // null means not found, empty set means empty set coverage
+    static Set<Integer> getMinCoverage(List<Pair<ArrayList<Pair<Integer, Integer>>, Set<Integer>>> cache, ArrayList<Pair<Integer, Integer>> queryInterval){
+
+        Set<Integer> result = null;
+        for (Pair<ArrayList<Pair<Integer, Integer>>, Set<Integer>> entry : cache){
+            if (intervalContainsInterval(entry.getLeft(), queryInterval) && (result == null || result.size() > entry.getRight().size())){
+                result = entry.getRight();
+            }
+        }
+
+        return result;
+
     }
 
 }
--- src/test/java/local/BenchmarkTest.java
+++ src/test/java/local/BenchmarkTest.java
@@ -1,32 +1,152 @@
 package local;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
-import static local.Benchmark.createRandomTable;
+import static local.Benchmark.*;
 
 public class BenchmarkTest {
 
     @Test
     public void createRandomTableTest(){
-        HashMap<Integer, List<List<Double>>> table = createRandomTable(10, 100_000, 100);
 
-        Assertions.assertEquals(100, table.size());
+        final int NUM_OF_FILES = 100;
+        final int NUM_OF_COLUMNS = 10;
+        final int NUM_OF_RECORDS = 100_000;
+
+        HashMap<Integer, List<ArrayList<Integer>>> table = createRandomTable(NUM_OF_COLUMNS, NUM_OF_RECORDS, NUM_OF_FILES);
+
+        Assertions.assertEquals(NUM_OF_FILES, table.size());
 
         int someKey = table.keySet().stream().findFirst().get();
-        List<List<Double>> somePartition = table.get(someKey);
+        List<ArrayList<Integer>> somePartition = table.get(someKey);
 
-        Assertions.assertTrue(!somePartition.isEmpty() && somePartition.size() < 100_000);
-        Assertions.assertEquals(10, somePartition.get(0).size());
+        Assertions.assertTrue(!somePartition.isEmpty() && somePartition.size() < NUM_OF_RECORDS);
+        Assertions.assertEquals(NUM_OF_COLUMNS, somePartition.get(0).size());
 
         int total = 0;
         for (Integer key : table.keySet()){
             total += table.get(key).size();
         }
-        Assertions.assertEquals(100_000, total);
+        Assertions.assertEquals(NUM_OF_RECORDS, total);
+
+    }
+
+    @Test
+    public void runRandomQueryTest(){
+
+        final int NUM_OF_FILES = 10000;
+        final int NUM_OF_COLUMNS = 10;
+        final int NUM_OF_RECORDS = 1_000_000;
+
+        HashMap<Integer, List<ArrayList<Integer>>> table = createRandomTable(NUM_OF_COLUMNS, NUM_OF_RECORDS, NUM_OF_FILES);
+
+        Integer result = runRandomQuery(table, false);
+
+        System.out.println(result);
+
+    }
+
+    @Test
+    public void generateRecordTest(){
+        List<Integer> record = generateRecord(5);
+        System.out.println(record);
+    }
+
+    @Test
+    public void generateIntervalTest(){
+        List<Pair<Integer, Integer>> interval = generateInterval(6, 0.5);
+        System.out.println(interval);
+    }
+    
+    @Test
+    public void intervalContainsRecordTest(){
+        ArrayList<Integer> record = new ArrayList<>(Arrays.asList(100, 20, 45, -4, 37));
+        Pair<Integer, Integer> all = Pair.of(Integer.MIN_VALUE, Integer.MAX_VALUE);
+        ArrayList<Pair<Integer, Integer>> interval = new ArrayList<>(Arrays.asList(all, all, all, all, all));
+
+        Assertions.assertTrue(intervalContainsRecord(interval, record));
+
+        interval.set(0, Pair.of(99,101));
+        Assertions.assertTrue(intervalContainsRecord(interval, record));
+
+        interval.set(1, Pair.of(-10,10));
+        Assertions.assertFalse(intervalContainsRecord(interval, record));
+
+    }
+
+    @Test
+    public void intervalContainsIntervalTest(){
+        Pair<Integer, Integer> all = Pair.of(Integer.MIN_VALUE, Integer.MAX_VALUE);
+        ArrayList<Pair<Integer, Integer>> interval1 = new ArrayList<>(Arrays.asList(all, all, all, all, all));
+        ArrayList<Pair<Integer, Integer>> interval2 = new ArrayList<>(Arrays.asList(all, all, all, all, all));
+
+        Assertions.assertTrue(intervalContainsInterval(interval1, interval1));
+        Assertions.assertTrue(intervalContainsInterval(interval1, interval2));
+
+        interval1.set(0, Pair.of(0,100));
+        Assertions.assertFalse(intervalContainsInterval(interval1, interval2));
+
+        interval2.set(0, Pair.of(-10,100));
+        Assertions.assertFalse(intervalContainsInterval(interval1, interval2));
+
+        interval2.set(0, Pair.of(5,100));
+        Assertions.assertTrue(intervalContainsInterval(interval1, interval2));
+
+        interval1.set(3, Pair.of(-10,10));
+        Assertions.assertFalse(intervalContainsInterval(interval1, interval2));
+
+        interval2.set(3, Pair.of(-10,10));
+        Assertions.assertTrue(intervalContainsInterval(interval1, interval2));
+
+    }
+
+    @Test
+    public void getMinCoverageTest(){
+
+        List<Pair<ArrayList<Pair<Integer, Integer>>, Set<Integer>>> cache = new LinkedList<>();
+
+        ArrayList<Pair<Integer, Integer>> interval1
+                = new ArrayList<>(Arrays.asList(Pair.of(0,10), Pair.of(20,1000), Pair.of(-10, -5)));
+
+        Set<Integer> coverage1 = new HashSet<>(Arrays.asList(10,20,30));
+
+        ArrayList<Pair<Integer, Integer>> interval2
+                = new ArrayList<>(Arrays.asList(Pair.of(Integer.MIN_VALUE,Integer.MAX_VALUE), Pair.of(20,1000), Pair.of(-10, -5)));
+
+        ArrayList<Pair<Integer, Integer>> interval3
+                = new ArrayList<>(Arrays.asList(Pair.of(0,0), Pair.of(0,10), Pair.of(-10, 0)));
+
+
+        Set<Integer> coverage2 = new HashSet<>(Arrays.asList(10,20,30,40));
+
+        cache.add(Pair.of(interval1, coverage1));
+        cache.add(Pair.of(interval2, coverage2));
+        cache.add(Pair.of(interval3, new HashSet<>()));
+
+
+        ArrayList<Pair<Integer, Integer>> myInterval1
+                = new ArrayList<>(Arrays.asList(Pair.of(Integer.MIN_VALUE,Integer.MAX_VALUE), Pair.of(30,50), Pair.of(-2, -1)));
+
+        ArrayList<Pair<Integer, Integer>> myInterval2
+                = new ArrayList<>(Arrays.asList(Pair.of(0,0), Pair.of(30,50), Pair.of(-8, -6)));
+
+        ArrayList<Pair<Integer, Integer>> myInterval3
+                = new ArrayList<>(Arrays.asList(Pair.of(-100,0), Pair.of(30,50), Pair.of(-8, -6)));
+
+        ArrayList<Pair<Integer, Integer>> myInterval4
+                = new ArrayList<>(Arrays.asList(Pair.of(0,0), Pair.of(2,3), Pair.of(-8, -6)));
+
+        Assertions.assertNull(getMinCoverage(cache, myInterval1));
+
+        Assertions.assertEquals(3, getMinCoverage(cache, myInterval2).size());
+
+        Assertions.assertEquals(4, getMinCoverage(cache, myInterval3).size());
+
+        Assertions.assertEquals(0, getMinCoverage(cache, myInterval4).size());
 
     }
 }
